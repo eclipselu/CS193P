0ab6c774e08aeb8d3cbb74605922d36de72d625b
eclipselu <eclipselu@gmail.com>
2012-03-06 22:26:45 +0800

    Assignment 2B

---
+++ Hello/HelloTests/HelloTests.h
@@ -0,0 +1,13 @@
+//
+//  HelloTests.h
+//  HelloTests
+//
+//  Created by Lu Dang on 3/6/12.
+//  Copyright (c) 2012 Zhejiang University. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HelloTests : SenTestCase
+
+@end
---
+++ Hello/HelloTests/HelloTests.m
@@ -0,0 +1,32 @@
+//
+//  HelloTests.m
+//  HelloTests
+//
+//  Created by Lu Dang on 3/6/12.
+//  Copyright (c) 2012 Zhejiang University. All rights reserved.
+//
+
+#import "HelloTests.h"
+
+@implementation HelloTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HelloTests");
+}
+
+@end
